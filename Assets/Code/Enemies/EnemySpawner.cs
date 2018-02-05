23698c24bd0f174e0664c4502ce447f55e9ee512
Unknown <fredriklovlie@gmail.com>
2018-02-05 22:49:26 +0100

    Cleaned up the code for the EnemySpawner

--- Assets/Code/Enemies/EnemySpawner.cs
+++ Assets/Code/Enemies/EnemySpawner.cs
@@ -12,14 +12,14 @@ public class SpawnData
 
 public class EnemySpawner : MonoBehaviour
 {
-    private float fCounter;
+    //private float fCounter;
 
     private float fTimer;
 
     private int i;
 
-    public GameObject xFly;
-    public GameObject xHoneyCombPickUp;
+    //public GameObject xFly;
+    //public GameObject xHoneyCombPickUp;
 
     public List<SpawnData> aSpawnData;
 
@@ -52,31 +52,31 @@ public class EnemySpawner : MonoBehaviour
 	    }
 	}
 
-    void SpawnFlies()
-    {
-        Vector2 v2SwarmCenter = this.transform.position;
-        v2SwarmCenter.y = Random.Range(-4, 4);
-        GameObject fly1 = Instantiate(xFly);
-        fly1.transform.position = v2SwarmCenter;
-        GameObject fly2 = Instantiate(xFly);
-        fly2.transform.position = v2SwarmCenter;
-        fly2.transform.Translate(1, 0, 0);
-        GameObject fly3 = Instantiate(xFly);
-        fly3.transform.position = v2SwarmCenter;
-        fly3.transform.Translate(0, 1, 0);
-        GameObject fly4 = Instantiate(xFly);
-        fly4.transform.position = v2SwarmCenter;
-        fly4.transform.Translate(0, -1, 0);
-        GameObject fly5 = Instantiate(xFly);
-        fly5.transform.position = v2SwarmCenter;
-        fly5.transform.Translate(-1, 0, 0);
-    }
+    //void SpawnFlies()
+    //{
+    //    Vector2 v2SwarmCenter = this.transform.position;
+    //    v2SwarmCenter.y = Random.Range(-4, 4);
+    //    GameObject fly1 = Instantiate(xFly);
+    //    fly1.transform.position = v2SwarmCenter;
+    //    GameObject fly2 = Instantiate(xFly);
+    //    fly2.transform.position = v2SwarmCenter;
+    //    fly2.transform.Translate(1, 0, 0);
+    //    GameObject fly3 = Instantiate(xFly);
+    //    fly3.transform.position = v2SwarmCenter;
+    //    fly3.transform.Translate(0, 1, 0);
+    //    GameObject fly4 = Instantiate(xFly);
+    //    fly4.transform.position = v2SwarmCenter;
+    //    fly4.transform.Translate(0, -1, 0);
+    //    GameObject fly5 = Instantiate(xFly);
+    //    fly5.transform.position = v2SwarmCenter;
+    //    fly5.transform.Translate(-1, 0, 0);
+    //}
 
-    void SpawnPickUp()
-    {
-        GameObject honeyCombPickUp = Instantiate(xHoneyCombPickUp);
-        Vector2 v2SpawnLocation = this.transform.position;
-        v2SpawnLocation.y = Random.Range(-4, 4);
-        honeyCombPickUp.transform.position = v2SpawnLocation;
-    }
+    //void SpawnPickUp()
+    //{
+    //    GameObject honeyCombPickUp = Instantiate(xHoneyCombPickUp);
+    //    Vector2 v2SpawnLocation = this.transform.position;
+    //    v2SpawnLocation.y = Random.Range(-4, 4);
+    //    honeyCombPickUp.transform.position = v2SpawnLocation;
+    //}
 }
