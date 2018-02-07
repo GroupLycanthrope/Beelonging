98f516971f286240d5b349b31c9d0c4af8cb9448
AdamOlsson97 <adaols@telia.com>
2018-02-07 20:40:36 +0100

    AI Bee shooting and detecting
    The AI bees can now shoot but do not shoot if the player is in front of them

--- Assets/Code/Bees/AI_Shooting.cs
+++ Assets/Code/Bees/AI_Shooting.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class AI_Shooting : MonoBehaviour {
 
+    //Bool
+    private bool PlayerInWay;
+    //Float
+    public float fFireRate;
+    public float fNextShot;
+    //Sound
+    public AudioClip acShootsound;
+    private AudioSource asSource;
+    //GameObj
     public GameObject goBulletStartPos;
-
     public GameObject goBullet;
-
-    Ray2D Sight;
-
+    //Raycasting
     float rayDist = 10;
-
     Vector3 dist;
    
     void Start () {
@@ -21,11 +26,28 @@ public class AI_Shooting : MonoBehaviour {
 	void Update () {
         RaycastHit2D hit = Physics2D.Raycast(goBulletStartPos.transform.position, Vector2.right, rayDist);
         Debug.DrawRay(goBulletStartPos.transform.position, dist, Color.red);
-        if(hit.collider != null) {
-
-            if (hit.collider.gameObject.name == "Player"){
-                Debug.Log(hit.collider.name);
+        if (hit == true) {
+           if(hit.collider.gameObject.name == "Player") {
+                PlayerInWay = true;
+           }else {
+                PlayerInWay = false;
             }
         }
+        else {
+            if(Time.time > fNextShot && PlayerInWay == false && GetComponentInChildren<Find_Move>().BeeCallerFormation == true) {
+                fNextShot = Time.time + fFireRate / 5;
+                GameObject bullet = Instantiate(goBullet);
+                bullet.transform.position = goBulletStartPos.transform.position;
+            }
+            else if (Time.time > fNextShot && PlayerInWay == false){
+                fNextShot = Time.time + fFireRate;
+                GameObject bullet = Instantiate(goBullet);
+                bullet.transform.position = goBulletStartPos.transform.position;
+            }
+        }
+    }
+
+    void stopShoot() {
+
     }
 }
