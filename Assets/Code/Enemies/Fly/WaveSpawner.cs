423d908c3bf10eacbdc534d03ce22846148196a6
Unknown <fredriklovlie@gmail.com>
2018-02-07 19:59:29 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Code/Enemies/Fly/WaveSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WaveSpawnData
+{
+    public Vector2 v2DeltaPosition;
+    public GameObject xSpawnObject;
+}
+public class WaveSpawner : MonoBehaviour
+{
+    public List<WaveSpawnData> aWaveSpawnData;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    for (int i = 0 ; i < aWaveSpawnData.Capacity ; i++)
+	    {
+	        GameObject spawn = Instantiate(aWaveSpawnData[i].xSpawnObject, this.transform);
+            spawn.transform.Translate(aWaveSpawnData[i].v2DeltaPosition);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
