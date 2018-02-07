7d712d002156dd0372bfcc5260689aa8dd8dffce
Unknown <fredriklovlie@gmail.com>
2018-02-07 18:21:50 +0100

    Added Dragonfly Movement and Collision
    plus added as prefab

---
+++ Assets/DragonFlyCollision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonFlyCollision : MonoBehaviour
+{
+    public float fHitPoints;
+
+    public int iScoreValue;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+    void OnCollisionEnter2D(Collision2D p_xOtherCollider)
+    {
+        if (p_xOtherCollider.gameObject.CompareTag("BeeBullet"))
+        {
+            if (fHitPoints <= p_xOtherCollider.gameObject.GetComponent<PlayerBullet>().fDamage)
+            {
+                GetComponent<SpriteRenderer>().enabled = false;
+                GetComponent<PolygonCollider2D>().enabled = false;
+                Destroy(gameObject, 1);
+                ScoreManager.iScore += iScoreValue;
+            }
+            else
+            {
+                fHitPoints -= p_xOtherCollider.gameObject.GetComponent<PlayerBullet>().fDamage;
+            }
+        }
+    }
+}
---
+++ Assets/DragonFlyMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonFlyMovement : MonoBehaviour
+{
+    public float fFlyingSpeed;
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate(-fFlyingSpeed * Time.deltaTime, 0, 0);
+	}
+}
