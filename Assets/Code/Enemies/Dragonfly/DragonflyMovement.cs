ea5dde0e7ba411a913dc5cad229aeab5d1c93382
Unknown <fredriklovlie@gmail.com>
2018-02-07 19:08:33 +0100

    Auto stash before merge of "master" and "origin/master"

--- Assets/Code/Enemies/Dragonfly/DragonflyMovement.cs
+++ Assets/Code/Enemies/Dragonfly/DragonflyMovement.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class DragonflyMovement : MonoBehaviour
 {
-    public float fFlyingSpeed;
+    public float fZoomSpeed;
+
+    public Vector2 v2RandomMinInterval;
+    public Vector2 v2RandomMaxInterval;
+
+    public float fZoomCooldown;
+
+    private float fNextZoom;
+
+    private Vector3 v3ZoomDirection;
 	// Use this for initialization
 	void Start ()
 	{
@@ -14,6 +23,24 @@ public class DragonflyMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.Translate(-fFlyingSpeed * Time.deltaTime, 0, 0);
+	    //if (transform.position.x < -20)
+	    //{
+     //       Destroy(gameObject);
+	    //}
+		//transform.Translate(-fFlyingSpeed * Time.deltaTime, 0, 0);
+	    if (Time.time > fNextZoom)
+	    {
+	        fNextZoom = Time.time + fZoomCooldown;
+	        float fRandomX = Random.Range(v2RandomMinInterval.x, v2RandomMaxInterval.x);
+	        float fRandomY = Random.Range(v2RandomMinInterval.y, v2RandomMaxInterval.y);
+	        v3ZoomDirection.x = transform.position.x - fRandomX;
+	        v3ZoomDirection.y = transform.position.y - fRandomY;
+	        v3ZoomDirection.Normalize();
+	    }
+
+	    if (transform.position.x > v3ZoomDirection.x)
+	    {
+            transform.Translate(v3ZoomDirection * fZoomSpeed * Time.deltaTime);
+	    }
 	}
 }
