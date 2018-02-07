290cf99872c044b6e5890f47f0c1cd2467aed4d7
Unknown <fredriklovlie@gmail.com>
2018-02-07 13:51:35 +0100

    Added SpiderShooting Script and added nullcheck for the enemy spawner
    also moved some scripts around

---
+++ Assets/Code/Enemies/Spider/SpiderShooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderShooting : MonoBehaviour
+{
+    public GameObject xSpiderProjectile;
+
+    public GameObject xPlayer;
+
+    public float fFireRate;
+    private float fNextShot;
+
+    private Vector2 v2AimDirection;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
