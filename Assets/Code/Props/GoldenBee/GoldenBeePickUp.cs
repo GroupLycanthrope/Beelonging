12229bc674f2616c0515b9e6a4355a2db7141b97
AdamOlsson97 <adaols@telia.com>
2018-02-25 16:54:58 +0100

    Golden_Bee Power Thing Implemented ish
    The Only Thing that cen be changed is the speed it travels the rest is automatic.
    Right now the AI bees that spawn does not start to wonder randomly I have no clue why

---
+++ Assets/Code/Props/GoldenBee/GoldenBeePickUp.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldenBeePickUp : MonoBehaviour {
+
+    public float fMoveSpeed;
+
+    float fDeathTimer;
+    float fDelay;
+
+    bool bCollidedWithPlayer;
+    bool bSpawnBee;
+
+    PolygonCollider2D pcCollider;
+    SpriteRenderer spRenderer;
+
+    GameObject AI_Bee;
+    GameObject BeeSpawnPos;
+
+    GameObject CreateBee;
+
+    void Start () {
+        pcCollider = GetComponent<PolygonCollider2D>();
+        spRenderer = GetComponent<SpriteRenderer>();
+        fDelay = 10;
+        fDeathTimer = fDelay;
+        AI_Bee = (GameObject)Resources.Load("BeeStuff/AI/AI_Bee", typeof(GameObject));
+        BeeSpawnPos = GameObject.Find("BeeSpawningPos");
+        bSpawnBee = false;
+    }
+
+	void Update () {
+        MovePickUp();
+        CheckBorder();
+
+        if (bCollidedWithPlayer) {
+            CollisionWithPlayer();
+        }
+	}
+
+    void MovePickUp() {
+        transform.Translate(Vector2.left * fMoveSpeed * Time.deltaTime);
+    }
+    void CheckBorder() {
+        if(transform.position.x <= -20) {
+            Destroy(gameObject);
+        }
+    }
+
+    void CollisionWithPlayer() {
+        pcCollider.enabled = false;
+        spRenderer.enabled = false;
+        fDeathTimer -= Time.deltaTime;
+
+        if (!bSpawnBee) {
+            CreateBee = Instantiate(AI_Bee);
+            CreateBee.transform.position = BeeSpawnPos.transform.position;
+            bSpawnBee = true;
+            BeeManager.AddBee(CreateBee);
+        }
+
+        if (fDeathTimer <= 0){
+            Destroy(gameObject);   
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.name == "Player") {
+            bCollidedWithPlayer = true;
+        }
+    }
+}
