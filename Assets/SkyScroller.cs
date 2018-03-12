4cc482783a9bb01d00f7e17e5e40ce1cde568aac
Unknown <fredriklovlie@gmail.com>
2018-03-12 22:49:26 +0100

    Skyscroller
    Now scrolls the sky dependant on the progress of the level
    
    Also fixed shooting of player in formation, no longer shoots the ammount of projectiles of bees in the swarm for the player (xd)

---
+++ Assets/SkyScroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Playables;
+
+public class SkyScroller : MonoBehaviour
+{
+
+
+    private MeshRenderer mMeshRenderer;
+
+    public float fScrollingSpeed;
+
+    private Vector2 v2TextureOffset;
+
+    // Use this for initialization
+    void Start()
+    {
+        mMeshRenderer = GetComponent<MeshRenderer>();
+        v2TextureOffset.x = 0;
+        v2TextureOffset = mMeshRenderer.material.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Spawner.GetProcentOfWave(Spawner.GetWaveAt()) / 100 > 17)
+        {
+            float repeat = Mathf.Repeat((Spawner.GetProcentOfWave(Spawner.GetWaveAt()) / 100), 1);
+            v2TextureOffset.x = repeat;
+            mMeshRenderer.material.SetTextureOffset("_MainTex", v2TextureOffset);
+        }
+    }
+
+    void OnDestroy()
+    {
+        v2TextureOffset.x = 0;
+        mMeshRenderer.material.SetTextureOffset("_MainTex", v2TextureOffset);
+    }
+
+}
